06b2f73f9814c37450934d9567f99205d2ab078f
Gibraan Rahman <grahman@eng.ucsd.edu>
2023-03-01 09:58:08 -0800

    Make collapse_data more efficient

--- st3/utils.py
+++ st3/utils.py
@@ -10,24 +10,13 @@ def collapse_data(
     source_name: str = "source",
     sink_name: str = "sink"
 ) -> (pd.DataFrame, pd.DataFrame):
-    source_tbl = []
-    md_grouped = (
-        metadata[metadata[sourcesink_column] == source_name]
-        .groupby(env_column)
-    )
-    for env, env_md in md_grouped:
-        env_samples = env_md.index
-        env_data = table.filter(env_samples, inplace=False).sum("observation")
-        env_data = pd.Series(env_data, name=env)
-        source_tbl.append(env_data)
-
-    source_tbl = pd.concat(source_tbl, axis=1).T.astype(int)
-    source_tbl = biom.Table(
-        source_tbl.T.values,
-        observation_ids=table.ids("observation"),
-        sample_ids=source_tbl.index
-    )
-
     sinks = metadata[metadata[sourcesink_column] == sink_name].index
+    training = metadata[metadata[sourcesink_column] == source_name].index
+
     sink_tbl = table.filter(sinks, inplace=False)
+    source_tbl = table.filter(training, inplace=False)
+
+    source_map = metadata[env_column].to_dict()
+    source_tbl = source_tbl.collapse(lambda s, m: source_map[s], norm=False)
+
     return source_tbl, sink_tbl
